f20bb829d1d389740e237978f2732306482aab3f
adlyrfu <adly19222@gmail.com>
2022-10-21 21:05:54 +0700

    Punyaku

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &#169; 2022 Adly Rosyad Fudhulul Ulwani</p>
+    </footer>
+  );
+};
+
+export default Footer;
